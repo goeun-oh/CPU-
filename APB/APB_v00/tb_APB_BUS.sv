30d7569f3ea967812095a4f3f54b21ee584d0f33
goeun-oh <103434703+goeun-oh@users.noreply.github.com>
2025-04-18 16:16:01 +0900

    tb_APB_BUS 모듈 추가 및 APB_Master와 APB_Slave 인스턴스화 구현

---
+++ APB/APB_v00/tb_APB_BUS.sv
@@ -0,0 +1,50 @@
+`timescale 1ns/1ps
+
+module tb_APB_BUS();
+    logic        PCLK;
+    logic        PRESET;
+    logic [31:0] PADDR;
+    logic [31:0] PWDATA;
+    logic        PENABLE;
+    logic        PWRITE;
+    logic        PSEL0;
+    logic        PSEL1;
+    logic        PSEL2;
+    logic        PSEL3;
+    logic [31:0] PRDATA0;
+    logic [31:0] PRDATA1;
+    logic [31:0] PRDATA2;
+    logic [31:0] PRDATA3;
+    logic        PREADY0;
+    logic        PREADY1;
+    logic        PREADY2;
+    logic        PREADY3;
+    logic        transfer;
+    logic        ready;
+    logic [31:0] addr;
+    logic [31:0] wdata;
+    logic [31:0] rdata;
+    logic        write;
+
+    APB_Master U_APB_Master(.*);
+
+    APB_Slave peripheral0 (
+        .*,
+        .PSEL(PSEL0),
+        .PRDATA(PRDATA0),
+        .PREADY(PREADY0)
+    );
+
+    always #5 PCLK = ~PCLK;
+
+    initial begin
+        PCLK =0; PRESET = 1;
+        #10 PRESET=0;
+        @(posedge PCLK);
+        #1 addr = 32'h1000_0000; write = 1; wdata = 10; transfer = 1;
+        @(posedge PCLK);
+        #1 transfer = 0;
+        wait(ready == 1'b1);
+        #20 $finish;
+    end
+endmodule
