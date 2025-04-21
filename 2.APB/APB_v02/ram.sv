2ff6656b565d8f0631d5fae614b04caf86d37d05
goeun-oh <103434703+goeun-oh@users.noreply.github.com>
2025-04-21 15:43:26 +0900

    Refactor APB architecture and implement GPI/GPO peripherals
    - Added APB_Master and APB_Slave modules to manage APB transactions.
    - Implemented GPI and GPO peripherals with appropriate registers and interfaces.
    - Created a ControlUnit and DataPath for the RV32I core to handle instruction execution.
    - Enhanced the MCU module to integrate GPI and GPO peripherals.
    - Updated the ROM module to load instructions from a memory file.
    - Defined instruction and operation codes in a separate defines.sv file.
    - Implemented a RAM module for data storage with APB interface.
    - Updated readme.md to reflect the new GPIO structure and register mapping.

---
+++ 2.APB/APB_v02/ram.sv
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+module ram (
+    input  logic        PCLK,
+    input  logic [11:0] PADDR,
+    input  logic [31:0] PWDATA,
+    input  logic        PWRITE,
+    input  logic        PENABLE,
+    input  logic        PSEL,
+    output logic [31:0] PRDATA,
+    output logic        PREADY
+);
+    logic [31:0] mem[0:2**10-1]; // RAM 주소: 0x1000_0000~0x1000_0fff 따라서 12 bit!
+
+    always_ff @( posedge PCLK ) begin
+        PREADY <= 1'b0;
+        if (PSEL & PENABLE) begin
+            PREADY <= 1'b1;
+            if (PWRITE) mem[PADDR[11:2]] <= PWDATA;
+            else PRDATA <= mem[PADDR[11:2]];
+        end
+    end
+
+endmodule
+   
---
+++ 2.APB/APB_v02/readme.md
@@ -0,0 +1,10 @@
+[GPIO 만들기 (GPI+GPO 합치기)]
+
+APB_SlaveInf에 IDR, ODR, MODER 이 있어야 한다.
+& inout buffer 만들기 (input과 output을 동시에 사용하기 위해서)
+
+MODER: 0x00
+IDR : 0x04
+ODR: 0x08
+
+이렇게 register 3개 쓰기!!
