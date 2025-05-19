b3be64647377f51b974e0e929bde285f4577235a
goeun-oh <103434703+goeun-oh@users.noreply.github.com>
2025-05-19 15:34:54 +0900

    SPI_Slave 모듈 인터페이스 추가 및 문서 개선

--- 3.AXILite/SPI/SPI_Slave.v
+++ 3.AXILite/SPI/SPI_Slave.v
@@ -1,7 +1,36 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
-module SPI_Slave(
-
+module SPI_Slave_Intf (
+    input        SCLK,
+    input        MOSI,
+    output       MISO,
+    input        SS,
+    output [7:0] wdata,
+    input  [7:0] rdata,
+    output [1:0] addr,
+    output       done
 );
+
+endmodule
+
+
+module SPI_RegisterFile (
+    input        write,
+    input  [1:0] addr,
+    input  [7:0] wdata,
+    output [7:0] rdata
+);
+    reg [7:0] slv_reg0, slv_reg1, slv_reg2, slv_reg3;
+
+    always @(*) begin
+        if (write) begin
+            case (addr)
+                2'b00: slv_reg0 = wdata;
+                2'b01: slv_reg1 = wdata;
+                2'b10: slv_reg2 = wdata;
+                2'b11: slv_reg3 = wdata;
+            endcase
+        end
+    end
 
 endmodule
