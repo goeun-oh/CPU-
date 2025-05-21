d301153f1d2b54e35f27ae0b2b379f75e791958f
goeun-oh <103434703+goeun-oh@users.noreply.github.com>
2025-05-21 16:10:18 +0900

    Add input and output ports to SPI_Master module for complete functionality

--- 3.AXILite/I2C/code/I2C_Master.v
+++ 3.AXILite/I2C/code/I2C_Master.v
@@ -1,8 +1,18 @@
 `timescale 1ns/1ps
 
 module SPI_Master(
-    input 
+    input clk,
+    input reset,
+    input [7:0] tx_data,
+    output tx_done,
+    output ready,
+    input start,
+    input i2c_en,
+    input stop,
+    output SCL,
+    inout SDA
 );
+
 
 
 endmodule
